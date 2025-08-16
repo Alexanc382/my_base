49dddd0438f4a6ae3283c3096c0ffebd19a73e68
Alexanc382 <alexanc382@gmail.com>
2025-08-16 14:10:46 +0300

    создан index.html и декоратор

--- ui/app.py
+++ ui/app.py
@@ -10,7 +10,7 @@ dir = 'D:\Alex\sql\my_base\data'
 file = 'employees.db'
 db_path = os.path.join(dir, file)
 
-# получаем даные из БД
+# получаем данные из БД
 def get_employees():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -19,3 +19,11 @@ def get_employees():
     conn.close()
     return employees
 
+
+@app.route('/') # декоратор. Когда польз. заходит на адрес /(гл.стр.) вызывается функция ниже
+def index(): # '/' - корень сайта
+    employees = get_employees() # достает данныз из базы
+    return render_template('index.html', employees=employees)
+
+if __name__ == '__main__':
+    app.run(debug=True)

7f5f5193b272d49056445ab3b12685f1301b6a21
Alexanc382 <alexanc382@gmail.com>
2025-08-16 14:34:26 +0300

    +

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ import sqlite3
 app = Flask(__name__)
 
 # путь к БД
-dir = 'D:\Alex\sql\my_base\data'
-file = 'employees.db'
-db_path = os.path.join(dir, file)
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # папка с app.py
+db_path = os.path.join(BASE_DIR, 'data', 'employees.db')
+print(db_path)
 # получаем данные из БД
 def get_employees():
     conn = sqlite3.connect(db_path)
@@ -22,7 +21,7 @@ def get_employees():
 
 @app.route('/') # декоратор. Когда польз. заходит на адрес /(гл.стр.) вызывается функция ниже
 def index(): # '/' - корень сайта
-    employees = get_employees() # достает данныз из базы
+    employees = get_employees() # достает данных из базы
     return render_template('index.html', employees=employees)
 
 if __name__ == '__main__':
